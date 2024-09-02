f311e5f78dce91f17ab02ad285963e7409413a24
hfgaser <hfgaser@gmail.com>
2024-09-02 14:50:02 +0300

    App ismi değiştirildi

--- zar/Form1.Designer.cs
+++ zar/Form1.Designer.cs
@@ -89,7 +89,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Dice Roll";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
